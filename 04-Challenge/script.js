9286b813b8e784cb6446a8591ae5b7809a84530e
Ethikry <113566829+Ethikry@users.noreply.github.com>
2023-01-14 04:16:36 -0700

    added more code for challenge 04

--- 04-Challenge/script.js
+++ 04-Challenge/script.js
@@ -199,7 +199,13 @@ function quizStop() {
 function displayScore() {
   submitPage.style.display = "none";
   hiScorePage.style.display = "block";
-  score.innerText = userScore;
+  score.innerHTML = userScore;
+
+  let username = initials.value;
+  let newScore = {username, userScore}
+
+  console.log(userScore);
+  console.log(score);
 }
 
 function displayHome() {
@@ -213,3 +219,40 @@ startBtn.addEventListener("click", quizStart);
 nextQuestionBtn.addEventListener("click", displayQuestions);
 submitBtn.addEventListener("click", displayScore);
 homeBtn.addEventListener("click", displayHome);
+
+
+function checkHighScore(score) {
+    const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
+    const lowestScore = highScores[NO_OF_HIGH_SCORES-1]?.score ?? 0;
+    
+    if (score > lowestScore) {
+      saveHighScore(score, highScores); // TODO
+      showHighScores(); // TODO
+    }
+  }
+
+  function saveHighScore(score, highScores) {
+    const name = prompt('You got a highscore! Enter name:');
+    const newScore = { score, name };
+    
+    // 1. Add to list
+    highScores.push(newScore);
+  
+    // 2. Sort the list
+    highScores.sort((a, b) => b.score - a.score);
+    
+    // 3. Select new list
+    highScores.splice(NO_OF_HIGH_SCORES);
+    
+    // 4. Save to local storage
+    localStorage.setItem(HIGH_SCORES, JSON.stringify(highScores));
+  };
+
+  function showHighScores() {
+    const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
+    const highScoreList = document.getElementById(HIGH_SCORES);
+    
+    highScoreList.innerHTML = highScores
+      .map((score) => `<li>${score.score} - ${score.name}`)
+      .join('');
+  }
